090bb51c9211e28f0c3747a8140bb30e675c728e
Martin Wragg <mdwragg@gmail.com>
2017-06-16 15:19:25 -0700

    Moving animation/scroll control out to a more 'app' opinionated component.

--- dist/px-branding-bar.js
+++ dist/px-branding-bar.js
@@ -1,2 +1,2 @@
-'use strict';(function(){Polymer({is:'px-branding-bar',properties:{counterValue:{type:Number,value:0,notify:true}},handleClick:function handleClick(event,detail,sender){this.increment()},increment:function increment(){this.counterValue++}})})();
+'use strict';(function(){Polymer({is:'px-branding-bar'})})();
 //# sourceMappingURL=px-branding-bar.js.map
