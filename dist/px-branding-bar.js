4198823d0fd40fd61c4002fcd0d6d8d1ebd758cb
Martin Wragg <mdwragg@gmail.com>
2017-06-28 17:01:39 -0700

    More cleanup, title property, min/max component height, cleaner demo.

--- dist/px-branding-bar.js
+++ dist/px-branding-bar.js
@@ -1,2 +1,6 @@
-'use strict';(function(){Polymer({is:'px-branding-bar'})})();
+'use strict';(function(){'use strict';Polymer({is:'px-branding-bar',properties:{/**
+       * The application title to display in the lefthand corner of the branding bar.
+       * Defaults to document.title unless specified.
+       * @type {String}
+       */applicationTitle:{type:String,value:function value(){return document.title}}}})})();
 //# sourceMappingURL=px-branding-bar.js.map
--- px-branding-bar.es6.js
+++ px-branding-bar.es6.js
@@ -1,5 +1,21 @@
 (function() {
+  'use strict';
+  
   Polymer({
-    is: 'px-branding-bar'
+    is: 'px-branding-bar',
+    properties:{
+
+      /**
+       * The application title to display in the lefthand corner of the branding bar.
+       * Defaults to document.title unless specified.
+       * @type {String}
+       */
+      applicationTitle:{
+        type: String,
+        value: function() {
+          return document.title;
+        }
+      }
+    }
   });
 })();
