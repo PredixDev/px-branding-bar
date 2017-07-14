711e93bc08e7029550ae10d847dab6db06f72684
Martin Wragg <mdwragg@gmail.com>
2017-07-14 13:22:22 -0700

    more tests.

--- test/px-branding-bar-tests.js
+++ test/px-branding-bar-tests.js
@@ -1,13 +1,50 @@
-describe('px-branding-bar checked behaviour', function () {
-  it('should be unchecked by default', function() {
-    var toggleElement = fixture('PxBrandingBarFixture');
-    expect(toggleElement).to.exist;
+describe('px-branding-bar default look & behaviour', function () {
+  it('fixture should exist', function() {
+    var brandingBarEl = fixture('PxBrandingBarFixture');
+    expect(brandingBarEl).to.exist;
   });
 
-  // it('can be checked by setting property', function() {
-  //   var toggleElement = fixture('PxBrandingBarFixture');
-  //   assert.isFalse(toggleElement.checked);
-  //   toggleElement.checked = true;
-  //   assert.isTrue(toggleElement.checked);
-  // });
+  it('element name should be correct', function() {
+    var brandingBarEl = fixture('PxBrandingBarFixture');
+    expect(brandingBarEl.tagName).to.equal('PX-BRANDING-BAR');
+  });
+
+  it('branding bar should have a GE logo by default', function() {
+    var brandingBarEl = fixture('PxBrandingBarFixture');
+    var geLogo = Polymer.dom(brandingBarEl.root).querySelector('px-ge-svg-logo');
+    expect(geLogo).to.exist;
+  });
+
+  it('branding bar property should have default title of document.title', function() {
+    var brandingBarEl = fixture('PxBrandingBarFixture');
+    expect(brandingBarEl.applicationTitle).to.equal(document.title);
+  });
+
+  it('branding bar should display default title of document.title', function() {
+    var brandingBarEl = fixture('PxBrandingBarFixture');
+    var titleEl = Polymer.dom(brandingBarEl.root).querySelector('label');
+    expect(titleEl.textContent).to.equal(document.title);
+  });
+
+});
+
+describe('px-branding-bar attributes setting', function () {
+
+  it('setting application-title attribute should change displayed title', function() {
+    var brandingBarEl = fixture('PxBrandingBarSetPropertiesFixture');
+    expect(brandingBarEl.applicationTitle).to.equal('Moar Predix Please');
+  });
+
+});
+
+describe('px-branding-bar custom slot content', function () {
+
+  it('setting title slot should change displayed title', function() {
+    var brandingBarEl = fixture('PxBrandingBarCustomContentFixture');
+    flush(function(done){
+      expect(brandingBarEl.applicationTitle).to.equal('Moar Predix Please');
+      done();
+    });
+  });
+
 });
